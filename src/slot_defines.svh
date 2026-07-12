c9ab5851925f7fb82e7c1dacd7f7221ab3de4c53
Julia Desmazes <julia.desmazes@gmail.com>
2026-07-12 00:00:34 -0500

    update pins

--- src/slot_defines.svh
+++ src/slot_defines.svh
@@ -56,8 +56,8 @@
 `define NUM_VSS_PADS 1
 
 // Signal pads
-`define NUM_INPUT_PADS 12
-`define NUM_BIDIR_PADS 33
+`define NUM_INPUT_PADS 16
+`define NUM_BIDIR_PADS 29
 `define NUM_ANALOG_PADS 1
 
 `endif
